60b312a1969160f0c336dd6dc43a7464d13e703d
tobias <tobiasurhaug@gmail.com>
2017-08-14 20:13:17 +0200

    fix apache commons gradle import

---
+++ src/main/java/LeapYear.java
@@ -0,0 +1,11 @@
+
+public class LeapYear
+{
+
+    public static boolean test(int year)
+    {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+}
---
+++ src/test/java/LeapYearTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class LeapYearTest
+{
+
+    @Test
+    public void a_julian_year_divisible_by_four_is_a_leap_year()
+    {
+	// assertThat(LeapYear.test(1580)).isTrue();
+    }
+
+}
